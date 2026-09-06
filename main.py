81822f574173c7b79f788270173877f6ac10e0db
Pollaghan <diavicecatgaminglive@gmail.com>
2026-09-06 15:27:24 +0100

    cheese

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+# init stuff
+pygame.init()
+pygame.display.set_caption("C H E E S E")
+screen = pygame.display.set_mode((800, 600))
+icon = pygame.image.load("Cheese.png")
+pygame.display.set_icon(icon)
+# TOO MANY VARIABLE
+Open = 1
+# the damn game loop
+while Open : (
+    screen.fill((0, 0, 0))
+)
