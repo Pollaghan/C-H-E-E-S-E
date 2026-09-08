8deef32e3778eebfa3f9d8c21a598f9bcf90255a
Pollaghan <diavicecatgaminglive@gmail.com>
2026-09-08 17:25:11 +0100

    cheese

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-import gif_pygame
+import threading
 # Initialisation crap
 pygame.init()
 pygame.display.set_caption("C H E E S E")
@@ -9,25 +9,45 @@ screen = pygame.display.set_mode((ResolutionX, ResolutionY), pygame.RESIZABLE, p
 icon = pygame.image.load("Cheese.png")
 pygame.display.set_icon(icon)
 # TOO MANY VARIABLES (yes that was a portal reference. deal with it)
-Cheese = gif_pygame.load("CHEESE.gif")
-# move
-s1 = pygame.Surface((20, 0))
-s2 = pygame.Surface((20, 0))
-s3 = pygame.Surface((20, 0))
-s1.fill((255, 0, 0))
-s2.fill((0, 255, 0))
-s3.fill((0, 0, 255))
-animation_surfs = gif_pygame.GIFPygame([[s1, 1], [s2, 1], [s3, 0.5]])
+frame = 1
+Player = pygame.image.load("PLAYER.png")
+Player = pygame.transform.scale(Player, (250, 250))
+PlayerY = (ResolutionY / 2) - Player.get_height() / 2
+# Functions n stuff
+def ImageSequence(SequenceFolder,FrameName, Destination, EndFrame):
+    Cheese = pygame.image.load(SequenceFolder + "/" + FrameName + "1" + ".png")
+    Cheese = pygame.transform.scale(Cheese, (300, 300))
+    if frame < EndFrame+1:
+        Cheese = pygame.image.load(SequenceFolder+"/"+FrameName+str(round(frame))+".png")
+        Cheese = pygame.transform.scale(Cheese, (300, 300))
+        screen.blit(Cheese, Destination)
+    else:
+        screen.blit(Cheese, Destination)
+    Cheese = pygame.transform.scale(Cheese, (300, 300))
+def Jump(Height) :
+    JumpHeight = 300
+    return JumpHeight
 # the damn game loop
 while True :
-    # Scaling
-    Cheese = pygame.transform.scale(Cheese, (200, 200))
     # RENDERINNNNNNG
     screen.fill((0, 0, 0))
     ResolutionY = pygame.display.get_surface().get_height()
     ResolutionX = pygame.display.get_surface().get_width()
-    screen.blit(Cheese,(ResolutionX-200,ResolutionY-200))
+    if frame < 48 :
+        CheeseRendering = threading.Thread(target=ImageSequence("CHESEE","ches", (ResolutionX-300, ResolutionY-300), 48))
+    else:
+        # where we now live THIS IS OUR NEW HOME
+        screen.blit(Player, ((ResolutionX/2)-Player.get_width()/2,PlayerY))
+        if PlayerY < 720 :
+            PlayerY += 1
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    PlayerY -= Jump(300)
+
+        # bye
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+    frame += 0.2
